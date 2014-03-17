9047e9902076df70b00c3368d266d9ba0bb3435f
wikis7 <edgar_alan_25@hotmail.com>
2014-03-17 16:57:42 -0600

    Repo UAF

---
+++ SIE-UAF-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SIE_UAF_2.Global" Language="C#" %>
---
+++ SIE-UAF-2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIE_UAF_2.Startup))]
+namespace SIE_UAF_2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
